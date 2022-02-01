fddea834cee73cee48761669e1a4f1ae90862dbf
Cornflower Guppy <ewen.fortune@gmail.com>
2022-02-01 09:26:12 +0000

    add brackets to map

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
     if params[:ratings].nil?
       @ratings_to_show = Movie.all_ratings
     else
-      @ratings_to_show = params[:ratings].map{|r| "r:1"}
+      @ratings_to_show = params[:ratings].map{|r| [r:1]}
     end
     if params[:sort_by].nil?
     @movies = Movie.with_ratings(@ratings_to_show)
